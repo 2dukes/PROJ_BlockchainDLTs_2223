b6e517c2de0bbd60be835052eff5430a18564b31
2dukes <ruipinto02@hotmail.com>
2022-12-07 22:12:12 +0100

    Add: First design of Dapp.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,85 @@
-import { Fragment } from "react";
-import Button from '@mui/material/Button';
-import { connectWallet } from "./scripts/connectWallet";
+import * as React from 'react';
+import { Fragment } from 'react';
+import { AppBar, Box, Toolbar, IconButton, Typography, Container } from '@mui/material';
+import { Search, SearchIconWrapper, StyledInputBase } from "./styles/Menu";
+import Grid from '@mui/material/Grid';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import CampaignCard from "./components/CampaignCard";
+// import { connectWallet } from "./scripts/connectWallet";
+
+const data = [
+  {
+    id: 1,
+    title: "Lizard 1",
+    description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+    imageURL: "https://img.capital.com/imgs/articles/662x308x1/shutterstock_1958528764.jpg",
+  },
+  {
+    id: 2,
+    title: "Lizard 2",
+    description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+    imageURL: "https://img.capital.com/imgs/articles/662x308x1/shutterstock_1958528764.jpg",
+  },
+  {
+    id: 3,
+    title: "Lizard 3",
+    description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+    imageURL: "https://img.capital.com/imgs/articles/662x308x1/shutterstock_1958528764.jpg",
+  },
+  {
+    id: 4,
+    title: "Lizard 4",
+    description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+    imageURL: "https://img.capital.com/imgs/articles/662x308x1/shutterstock_1958528764.jpg",
+  },
+];
 
 function App() {
   return (
     <Fragment>
-      <Button variant="contained" onClick={connectWallet}>Connect Wallet</Button>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h5"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            >
+              Dapp: Crowdfunding Reinvented
+            </Typography>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </Search>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Container maxWidth="lg">
+        <Typography variant="h2" marginTop="0.5em" textAlign="center" gutterBottom >
+          Campaigns
+        </Typography>
+        <Grid container
+          alignItems="center"
+          justify="center" spacing={3}>
+          {data.map(item => <Grid item xs={6} key={item.id}><CampaignCard title={item.title} description={item.description} imageURL={item.imageURL} /></Grid>)}
+        </Grid>
+      </Container>
     </Fragment>
   );
 }
---
+++ frontend/src/components/CampaignCard.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+const Campaign = ({ title, description, imageURL }) => {
+    return (
+        <Card sx={{ maxWidth: 500, margin: "auto" }}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image={imageURL}
+                    alt="green iguana"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {description}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    );
+};
+
+export default Campaign;
