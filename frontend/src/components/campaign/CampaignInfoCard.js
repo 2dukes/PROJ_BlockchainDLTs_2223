48be2e6dcc502cd20894cadae38b87be00847e6c
2dukes <ruipinto02@hotmail.com>
2022-12-12 16:35:31 +0100

    Update: Campaign modal with tooltip for the address of the campaign manager.

--- frontend/src/components/campaign/CampaignInfoCard.js
+++ frontend/src/components/campaign/CampaignInfoCard.js
@@ -1,13 +1,30 @@
 import { Card, CardContent, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
+import Zoom from '@mui/material/Zoom';
 
+const CustomWidthTooltip = styled(({ className, ...props }) => (
+    <Tooltip {...props} classes={{ popper: className }} />
+))({
+    [`& .${tooltipClasses.tooltip}`]: {
+        maxWidth: 150,
+    },
+});
 
 const CampaignInfoCard = ({ header, meta, description, isSmall }) => {
+    const isAddress = String(header).startsWith("0x");
+
     return (
         <Card style={{ height: '100%' }}>
             <CardContent sx={{ p: 1 }}>
-                <Typography noWrap variant={isSmall ? "h6" : "h5"} component="div">
-                    {header}
-                </Typography>
+                {isAddress ? (
+                    <CustomWidthTooltip TransitionComponent={Zoom} title={header} placement="bottom" arrow>
+                        <Typography noWrap variant={isSmall ? "h6" : "h5"} component="div">
+                            {header}
+                        </Typography>
+                    </CustomWidthTooltip>) : (<Typography noWrap variant={isSmall ? "h6" : "h5"} component="div">
+                        {header}
+                    </Typography>)}
                 <Typography sx={{ mb: isSmall ? 0 : 1.5 }} color="text.secondary">
                     {meta}
                 </Typography>
