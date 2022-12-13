ede4059936012361d299e9f6e4543b5913266cc0
2dukes <ruipinto02@hotmail.com>
2022-12-13 18:30:03 +0100

    Fix: DevTools bug in details tab. | Add: Dialog to buy product aciton.

--- frontend/src/components/campaign/CampaignCardWithPrice.js
+++ frontend/src/components/campaign/CampaignCardWithPrice.js
@@ -1,45 +1,59 @@
-import * as React from 'react';
+import { useState, Fragment } from 'react';
 import { Box, Chip, Button, Card, CardContent, CardMedia, Typography, CardActionArea } from '@mui/material';
+import ProductDeliveryForm from '../product/ProductDeliveryForm';
 
 const CampaignCardWithPrice = ({ title, description, price, imageURL }) => {
+    const [openDialog, setOpenDialog] = useState(false);
+
+    const handleClickOpenDialog = () => {
+        setOpenDialog(true);
+    };
+
+    const handleCloseDialog = () => {
+        setOpenDialog(false);
+    };
+
     return (
-        <Card sx={{ maxWidth: 550 }}>
-            <CardActionArea>
-                <CardMedia
-                    component="img"
-                    height="140"
-                    image={imageURL}
-                    alt="green iguana"
-                />
-                <CardContent>
-                    <Chip label="FEATURED" color="primary" size="small" sx={{ mb: "0.75em", borderRadius: 0 }} />
-                    <Typography gutterBottom variant="h6" component="div" fontWeight="bold">
-                        {title}
-                    </Typography>
-                    <Typography gutterBottom variant="h6" component="div" mb="0" paddingBottom="0.5em">
-                        <b>{price}</b> ETH <span style={{ color: "red", fontSize: "15px" }}><del>15 ETH</del>&nbsp;(33% OFF)</span>
-                        <Typography variant="body2" component="div" color="gray">
-                            $10&nbsp;303,70
+        <Fragment>
+            {openDialog && <ProductDeliveryForm open={openDialog} handleClose={handleCloseDialog} />}
+            <Card sx={{ maxWidth: 550 }}>
+                <CardActionArea component="a">
+                    <CardMedia
+                        component="img"
+                        height="140"
+                        image={imageURL}
+                        alt="green iguana"
+                    />
+                    <CardContent>
+                        <Chip label="FEATURED" color="primary" size="small" sx={{ mb: "0.75em", borderRadius: 0 }} />
+                        <Typography gutterBottom variant="h6" component="div" fontWeight="bold">
+                            {title}
                         </Typography>
-                    </Typography>
-                    <Typography variant="body2" component="div" sx={{ my: 1.5 }}>
-                        {description}
-                    </Typography>
-                    <Typography variant="body1" component="div" paddingBottom="0.75em">
-                        <b>23</b> already claimed.
-                    </Typography>
-                    <Typography variant="body1" component="div">
-                        Ships worldwide.
-                    </Typography>
-                    <Box sx={{ mt: 2 }}>
-                        <Button variant="outlined" sx={{ width: "100%" }}>
-                            Buy Product
-                        </Button>
-                    </Box>
-                </CardContent>
-            </CardActionArea>
-        </Card>
+                        <Typography gutterBottom variant="h6" component="div" mb="0" paddingBottom="0.5em">
+                            <b>{price}</b> ETH <span style={{ color: "red", fontSize: "15px" }}><del>15 ETH</del>&nbsp;(33% OFF)</span>
+                            <Typography variant="body2" component="div" color="gray">
+                                $10&nbsp;303,70
+                            </Typography>
+                        </Typography>
+                        <Typography variant="body2" component="div" sx={{ my: 1.5 }}>
+                            {description}
+                        </Typography>
+                        <Typography variant="body1" component="div" paddingBottom="0.75em">
+                            <b>23</b> already claimed.
+                        </Typography>
+                        <Typography variant="body1" component="div">
+                            Ships worldwide.
+                        </Typography>
+                        <Box sx={{ mt: 2 }}>
+                            <Button variant="outlined" onClick={handleClickOpenDialog} sx={{ width: "100%" }}>
+                                Buy Product
+                            </Button>
+                        </Box>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </Fragment>
     );
 };
 
-export default CampaignCardWithPrice;
+export default CampaignCardWithPrice;;
--- frontend/src/components/product/ProductDeliveryForm.js
+++ frontend/src/components/product/ProductDeliveryForm.js
@@ -1,43 +1,74 @@
-import { Grid, Typography, TextField, FormControl } from '@mui/material';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
 
-const ProductDeliveryForm = () => {
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const ProductDeliveryForm = ({ open, handleClose }) => {
     return (
-        <FormControl variant="outlined" sx={{ width: "100%" }}>
-            <Grid container
-                alignItems="center"
-                justify="center" columnSpacing={3}>
-                <Grid item xs={12} md={6}>
-                    <Typography variant="body1" fontWeight="bold">
-                        Name
-                    </Typography>
-                    <TextField
-                        id="title"
-                        sx={{ width: "100%" }}
-                    // value={values.campaignTitle}
-                    // onChange={() => {}}
-                    />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <Typography variant="body1" fontWeight="bold">
-                        Email
-                    </Typography>
-                    <TextField
-                        id="title"
-                        sx={{ width: "100%" }}
-                    // value={values.campaignTitle}
-                    // onChange={() => {}}
-                    />
-                </Grid>
-            </Grid>
-            <Typography variant="body1" fontWeight="bold" marginTop="1em">
-                Address
-            </Typography>
-            <TextField
-                id="description"
-            // value={values.campaignDescription}
-            // onChange={handleChange("campaignDescription")}
-            />
-        </FormControl>);
+        <Dialog
+            open={open}
+            TransitionComponent={Transition}
+            keepMounted
+            onClose={handleClose}
+            aria-describedby="alert-dialog-slide-description"
+        >
+            <DialogTitle>{"Use Google's location service?"}</DialogTitle>
+            <DialogContent>
+                <DialogContentText id="alert-dialog-slide-description">
+                    Let Google help apps determine location. This means sending anonymous
+                    location data to Google, even when no apps are running.
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Disagree</Button>
+                <Button onClick={handleClose}>Agree</Button>
+            </DialogActions>
+        </Dialog>
+        // <FormControl variant="outlined" sx={{ width: "100%" }}>
+        //     <Grid container
+        //         alignItems="center"
+        //         justify="center" columnSpacing={3}>
+        //         <Grid item xs={12} md={6}>
+        //             <Typography variant="body1" fontWeight="bold">
+        //                 Name
+        //             </Typography>
+        //             <TextField
+        //                 id="title"
+        //                 sx={{ width: "100%" }}
+        //             // value={values.campaignTitle}
+        //             // onChange={() => {}}
+        //             />
+        //         </Grid>
+        //         <Grid item xs={12} md={6}>
+        //             <Typography variant="body1" fontWeight="bold">
+        //                 Email
+        //             </Typography>
+        //             <TextField
+        //                 id="title"
+        //                 sx={{ width: "100%" }}
+        //             // value={values.campaignTitle}
+        //             // onChange={() => {}}
+        //             />
+        //         </Grid>
+        //     </Grid>
+        //     <Typography variant="body1" fontWeight="bold" marginTop="1em">
+        //         Address
+        //     </Typography>
+        //     <TextField
+        //         id="description"
+        //     // value={values.campaignDescription}
+        //     // onChange={handleChange("campaignDescription")}
+        //     />
+        // </FormControl>
+    );
 };
 
 export default ProductDeliveryForm;
