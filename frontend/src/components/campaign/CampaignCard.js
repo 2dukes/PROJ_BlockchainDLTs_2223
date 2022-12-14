4016d9308bee5b7205c6074ed736de4b0ec6c6eb
2dukes <ruipinto02@hotmail.com>
2022-12-14 16:35:12 +0100

    Add: Progress bar and clock with days left to Campaign card.

--- frontend/src/components/campaign/CampaignCard.js
+++ frontend/src/components/campaign/CampaignCard.js
@@ -1,11 +1,34 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import { Stack, Box, Typography, Card, CardMedia, CardContent, CardActionArea, LinearProgress } from '@mui/material';
+import WatchLaterIcon from '@mui/icons-material/WatchLater';
+import PropTypes from 'prop-types';
+
+const LinearProgressWithLabel = (props) => {
+    return (
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Box sx={{ width: '100%', mr: 1 }}>
+                <LinearProgress variant="determinate" {...props} />
+            </Box>
+            <Box sx={{ minWidth: 35 }}>
+                <Typography variant="body2" color="text.secondary">{`${Math.round(
+                    props.value,
+                )}%`}</Typography>
+            </Box>
+        </Box>
+    );
+};
+
+LinearProgressWithLabel.propTypes = {
+    /**
+     * The value of the progress indicator for the determinate and buffer variants.
+     * Value between 0 and 100.
+     */
+    value: PropTypes.number.isRequired,
+};
 
 const CampaignCard = ({ title, description, imageURL, setModalOpen }) => {
+    const progress = 25;
+
     return (
         <Card sx={{ maxWidth: 500, margin: "auto" }}>
             <CardActionArea onClick={() => setModalOpen(true)}>
@@ -19,9 +42,18 @@ const CampaignCard = ({ title, description, imageURL, setModalOpen }) => {
                     <Typography gutterBottom variant="h5" component="div">
                         {title}
                     </Typography>
-                    <Typography variant="body2" color="text.secondary">
+                    <Typography variant="body2" color="text.secondary" sx={{ mb: "1em" }}>
                         {description}
                     </Typography>
+                    <Typography variant="body2" component="span" color="text.secondary">
+                        <b>2 ETH</b> raised
+                    </Typography>
+                    <LinearProgressWithLabel value={progress} />
+                    <Stack direction="row" alignItems="center" gap={1} marginTop="0.5em">
+                        <WatchLaterIcon color="disabled"/> 3 days left
+                        <Typography variant="body2" component="div" color="text.secondary">
+                        </Typography>
+                    </Stack>
                 </CardContent>
             </CardActionArea>
         </Card>
